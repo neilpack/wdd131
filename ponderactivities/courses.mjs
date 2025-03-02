1bc9257138f9202a935ec9055fff48dd50d6a229
neilpack <orangemustang11@gmail.com>
2025-03-01 22:37:45 -0700

    finished part one

---
+++ ponderactivities/courses.mjs
@@ -0,0 +1,36 @@
+const aCourse = {
+    code: "CSE121b",
+    name: "Javascript Language",
+    sections: [
+      {
+        sectionNum: 1,
+        roomNum: "STC 353",
+        enrolled: 26,
+        days: "TTh",
+        instructor: "Bro T",
+      },
+      {
+        sectionNum: 2,
+        roomNum: "STC 347",
+        enrolled: 25,
+        days: "TTh",
+        instructor: "Sis A",
+      },
+    ],
+};  
+console.log(aCourse.code);
+
+function renderSections(sections) {
+    const html = sections.map(
+      (section) => `<tr>
+        <td>${section.sectionNum}</td>
+        <td>${section.roomNum}</td>
+        <td>${section.enrolled}</td>
+        <td>${section.days}</td>
+        <td>${section.instructor}</td></tr>`
+    );
+    document.querySelector("#sections").innerHTML = html.join("");
+  }
+  export default aCourse;
+
+
